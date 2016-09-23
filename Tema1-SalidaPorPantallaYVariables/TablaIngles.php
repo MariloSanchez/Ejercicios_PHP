7b334a57f49bfaf3e3dfbebadb01992a7020f588
Marilo Sanchez <Marilo Sanchez>
2016-09-23 10:54:56 +0200

    sibidos ejercicios tema 1

---
+++ Tema1-SalidaPorPantallaYVariables/TablaIngles.php
@@ -0,0 +1,35 @@
+<!DOCTYPE html>
+<!--
+To change this license header, choose License Headers in Project Properties.
+To change this template file, choose Tools | Templates
+and open the template in the editor.
+-->
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title></title>
+        <style>
+            table, tr, td{
+                border: 1px solid pink; 
+            }
+        </style>
+    </head>
+    <body>
+        <table>
+        <?php
+                echo "<tr>";
+                    echo "<td>Blue</td><td>Azul</td>";
+                echo "</tr>";
+                echo "<tr>";
+                    echo "<td>Pink</td><td>Rosa</td>";
+                echo "</tr>";
+                echo "<tr>";
+                    echo "<td>Green</td><td>Verde</td>";
+                echo "</tr>";
+                echo "<tr>";
+                    echo "<td>Purple</td><td>Morado</td>";
+                echo "</tr>";           
+        ?>
+            </table>
+    </body>
+</html>
